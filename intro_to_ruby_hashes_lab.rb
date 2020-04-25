df4230e34b4c4201e9a566c67c6a0cead8578d6b
adeeba reaz <adeebarivera@outlook.com>
2020-04-25 01:19:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,27 +1,50 @@
-def new_hash
+# def new_hash
 
-end
+# end
 
-def actor
+# def actor
 
-end
+# end
 
-def monopoly
-	monopoly = {}
+# def monopoly
+# 	monopoly = {}
 
-end
+# end
 
-def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+# def monopoly_with_second_tier
+# 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
-end
+# end
 
-def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+# def monopoly_with_third_tier
+# 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
-end
+# end
 
-def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+# def monopoly_with_fourth_tier
+# 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
-end
+# end
+
+
+contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_ice_cream_flavors: ["chocolate", "vanilla"]  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
+#add ice cream flavor to hash array
+
+contacts["Jon Snow"][:favorite_ice_cream_flavors] << "mint chip"
+
+#add key/value pairs
+hash = {first: "first value!", second: "second value!"}
+hash[:third] = "third value!"
+ 
+puts hash
+
